ef8b9d053f2fab4d3ed77592cb4ceb374ffad044
abinawheeler <abinawheeler@terences-macbook.home>
2020-12-09 23:11:14 +0100

    Tidy up

--- lib/file_parser.rb
+++ lib/file_parser.rb
@@ -1,40 +1,27 @@
-# Opens webserver.log file to get log entries and call the dissection class
+# frozen_string_literal: true
 
 require_relative 'line_dissection'
 require_relative 'page_visits_counter'
 require_relative 'unique_views_counter'
 require_relative 'file_not_found'
 
-
-
+# Opens webserver.log file to get log entries and call the dissection class
 class FileParser
-  
-
   def parse(file_name, option)
-    
     check_file_name(file_name)
-   
-    #use File.foreach to read file line by line. File.read would read the whole file 
-    #into memory at once
+    # use File.foreach to read file line by line. File.read would read the whole file
+    # into memory at once
     File.foreach(file_name) do |line|
-    
       dissected_line = LineDissection.new(line.chomp)
       option.add_to_count(dissected_line)
-      
     end
-    
   end
-  
 
   private
 
   def check_file_name(file_name)
     return if File.exist?(file_name)
-  
+
     raise FileNotFound, "Please check your file name, the file you want to parse <#{file_name}> doesn't exist"
   end
 end
-
-
-
-
--- lib/page_visits_counter.rb
+++ lib/page_visits_counter.rb
@@ -1,26 +1,24 @@
-require 'byebug'
+# frozen_string_literal: true
+
 # contains a hash which stores page paths(k) and a tally of visits(v)
-# prints a list 
 class PageVisitsCounter
-  
   def initialize
-    #create a new hash and set default value for any key to 0 so you can perform +=1 (nil doesn't understand how to ++ itself.)
+    # create a new hash and set default value for any key to 0
+    # so you can perform +=1 (nil doesn't understand how to ++ itself.)
     @page_counts = Hash.new(0)
   end
 
   def add_to_count(dissected_line)
-    #add a new key and one count to the value or add one count to existing key value pair
+    # add a new key and one count to the value or add one count to existing key value pair
     page_counts[dissected_line.path] += 1
   end
 
   def print_result
-    #print out pages in decending order showing counts and path 
-    page_counts.sort_by { |page, count| [-count] }.map { |page, count| "#{count} visits to #{page}" }.join("\n")    
+    # print out pages in decending order showing counts and path
+    page_counts.sort_by { |page, count| [-count] }.map { |page, count| "#{count} visits to #{page}" }.join("\n")
   end
 
-  
   private
 
   attr_accessor :page_counts
 end
-
