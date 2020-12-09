6b48d0f76b50db6f864c43c43149198f3c0aee55
abinawheeler <abinawheeler@terences-macbook.home>
2020-12-09 22:16:46 +0100

    Tidy up

--- Gemfile
+++ Gemfile
@@ -1,18 +1,17 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 ruby '2.6.3'
 
-
 group :test do
-  gem 'rspec'
   gem 'coderay'
+  gem 'rspec'
 end
-  
 
 group :development do
   gem 'byebug'
+  gem 'rubocop', require: false
 end
--- bin/parser.rb
+++ bin/parser.rb
@@ -1,4 +1,5 @@
-#Ruby class for command-line option analysis
+# frozen_string_literal: true
+
 require 'optparse'
 
 require_relative '../lib/file_parser'
@@ -6,66 +7,33 @@ require_relative '../lib/unique_views_counter'
 require_relative '../lib/page_visits_counter'
 require_relative '../lib/file_not_found'
 
+options = {}
 
+# By default count page visits
+options[:count_type] = PageVisitsCounter.new
 
-class Parser
+option_parser = OptionParser.new do |opts|
+  opts.banner = 'Usage: ruby bin/parser.rb webserver.log --count_type [number_page_visits/page_unique_views]'
 
-  def self.parse(args)
-    
-    # This hash will hold all of the options
-    # parsed from the command-line by
-    # OptionParser.
-    options = {}
-
-    opt_parser = OptionParser.new do |opts|
-      
-      opts.banner = "Usage: ruby bin/parser.rb webserver.log --count_type [number_page_visits/page_unique_views]"
-
-      opts.on('-ct', '--count_type COUNT_TYPE', %i[number_page_visits page_unique_views]) do |count_type|
-
-        if count_type == :number_page_visits
-          options[:count_type] = PageVisitsCounter.new 
-          
-        
-        elsif count_type == :page_unique_views
-          options[:count_type] = UniqueViewsCounter.new 
-        end
-      end
-        
-      opts.on("-h", "--help", "Prints this help") do
-        puts opts
-        exit
-      end    
-
-    end
-    
-    
-    begin
-      opt_parser.parse!(options)
-      return options
-    rescue StandardError => e
-      # Only the app's exceptions are swallowed. Things like SyntaxErrror are left alone. 
-      puts "Exception encountered: #{e}"
-      exit 1
-    end
-    
+  opts.on('-ct', '--count_type COUNT_TYPE', %i[number_page_visits page_unique_views]) do |count_type|
+    options[:count_type] = PageVisitsCounter.new if count_type == :number_page_visits
+    options[:count_type] = UniqueViewsCounter.new if count_type == :page_unique_views
   end
-
+  opts.on('-h', '--help', 'Prints this help') do
+    puts opts
+    exit
+  end
 end
 
-
+# parse! theis will remove the switches destructivly ie you just have the file webserver.log
+option_parser.parse!
+file_name = ARGV[0]
+counts = options[:count_type]
 
 begin
-  options = Parser.parse(ARGV)
-  file_name = ARGV[0]
-  file_parser = FileParser.new
-  file_parser.parse(file_name,  options[:count_type]) 
-  
+  FileParser.new.parse(file_name, counts)
+
   puts options[:count_type].print_result
 rescue FileNotFound => e
   puts e
 end
-  
-
-
-
